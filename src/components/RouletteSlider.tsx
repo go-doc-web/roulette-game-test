bf9ab61f6c15f0b7a5e0ecca128d05de6d36dcca
Oleh Hubskiy <gubskyolegod@gmail.com>
2025-06-03 18:02:06 +0300

    refactor:moves the variable outside the function

--- src/components/RouletteSlider.tsx
+++ src/components/RouletteSlider.tsx
@@ -15,13 +15,13 @@ const iconMap = {
   jockerRed: <JockerSvg color="--fishka-red" />,
 };
 
+const placeholderChips = (
+  <div className="border-t-2 bg-yellow-500 border-solid border-white/20 w-[100px] h-[100px] flex items-center justify-center rounded-[0.5rem] text-red-500 text-3xl font-bold">
+    ?
+  </div>
+);
+
 export default function RouletteSlider({ cells }: RouletteSliderProps) {
-  console.log("cells", cells);
-  const placeholderChips = (
-    <div className="border-t-2 bg-yellow-500 border-solid border-white/20 w-[100px] h-[100px] flex items-center justify-center rounded-[0.5rem] text-red-500 text-3xl font-bold">
-      ?
-    </div>
-  );
   return (
     <div className="relative w-full  ">
       <div className="absolute inset-y-0 left-1/2 w-1 border-l-4 border-yellow-300 z-10"></div>
