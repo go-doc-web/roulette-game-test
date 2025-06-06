198f79a52bc63d260d572da635523be49a425b1b
Oleh Hubskiy <gubskyolegod@gmail.com>
2025-06-07 00:32:00 +0300

    fix error v

--- src/app/api/roulette/route.ts
+++ src/app/api/roulette/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
+// import prisma from "@/lib/prisma";
 import buildCells from "../../../lib/buildCells";
 import { rouletteSequence } from "../../../lib/rouletteSequence";
 import {
