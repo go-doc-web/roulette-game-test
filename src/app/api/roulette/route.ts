a24ea53f0c071280bac298c23aba5a687c330bb3
Oleh Hubskiy <gubskyolegod@gmail.com>
2025-06-07 13:57:38 +0300

    added POST recerd winning number and color to base

--- src/app/api/roulette/route.ts
+++ src/app/api/roulette/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-// import prisma from "@/lib/prisma";
+import prisma from "@/lib/prisma";
 import buildCells from "../../../lib/buildCells";
 import { rouletteSequence } from "../../../lib/rouletteSequence";
 import {
@@ -26,12 +26,7 @@ export async function GET() {
     console.log("winningCellColor", winningCellColor);
     console.log("winningNumber", winningNumber);
     console.log("winningColor", winningColor);
-    console.log({
-      sequence,
-      winnerIndex,
-      winningNumber,
-      winningColor,
-    });
+
     return NextResponse.json({
       sequence,
       winnerIndex,
@@ -50,9 +45,65 @@ export async function GET() {
   }
 }
 
-// export async function POST(request: Request) {
-//   try {
-//     const body = await request.json();
-//     const { number, color } = body;
-//   } catch (error) {}
-// }
+export async function POST(request: Request) {
+  try {
+    const { number, color } = await request.json();
+    if (
+      typeof number !== "number" ||
+      number < 0 ||
+      number > 14 ||
+      typeof color !== "string" ||
+      !["green", "red", "black"].includes(color)
+    ) {
+      return NextResponse.json(
+        {
+          error:
+            'Invalid data provided. Number must be 0-14, color must be "green", "red", or "black".',
+        },
+        { status: 400 }
+      );
+    }
+
+    // Save gameRound to base
+
+    const newGameRound = await prisma.gameRound.create({
+      data: {
+        number,
+        color,
+      },
+    });
+
+    //Garbage Collection
+    const maxRecords = 500;
+    const count = await prisma.gameRound.count();
+    if (count > maxRecords) {
+      const recordsToDelete = count - maxRecords;
+
+      const oldestRecords = await prisma.gameRound.findMany({
+        orderBy: {
+          timestamp: "asc",
+        },
+        take: recordsToDelete,
+        select: { id: true },
+      });
+
+      await prisma.gameRound.deleteMany({
+        where: {
+          id: {
+            in: oldestRecords.map((record: { id: string }) => record.id),
+          },
+        },
+      });
+      console.log(
+        `[API /api/roulette] Deleted ${oldestRecords.length} old game rounds to maintain limit of ${maxRecords}.`
+      );
+    }
+    return NextResponse.json(newGameRound, { status: 201 });
+  } catch (error) {
+    console.error("[API /api/roulette] Error saving game round:", error);
+    return NextResponse.json(
+      { error: "Failed to save game round due to a server error." },
+      { status: 500 }
+    );
+  }
+}
